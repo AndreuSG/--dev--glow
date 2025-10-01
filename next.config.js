147575692018ad5dd4547110ca7de7360c08fafc
Andreu <andreu.s.g30@gmail.com>
2025-10-01 12:33:41 +0200

    feat: update next.config.js with basePath, assetPrefix, and trailingSlash settings

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   output: 'export',
+  basePath: "/glow",
+  assetPrefix: "/glow/",
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
-  webpack: (config, { dev, isServer }) => {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config, { dev }) => {
     // Disable webpack caching in development to prevent file system issues
     if (dev) {
       config.cache = false;
