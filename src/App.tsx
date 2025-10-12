1eac7fc9960f1ef10224a88ad1cb5f81d2ce214c
Andreu Sánchez Guerrero <andreu.s.g30@gmail.com>
2025-10-12 14:50:43 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import { Navbar } from './components/layout/navbar';
 import { Footer } from './components/layout/footer';
 import Home from './pages/Home';
@@ -7,9 +8,20 @@ import Productos from './pages/Productos';
 import Equipo from './pages/Equipo';
 import Contacto from './pages/Contacto';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <BrowserRouter basename={import.meta.env.PROD ? '/glow' : ''}>
+      <ScrollToTop />
       <div className="min-h-screen flex flex-col">
         <Navbar />
         <Routes>
