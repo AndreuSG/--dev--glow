10fe09cfc9c54e0cbaad3313eaedc2ea8da2d2c7
Andreu <andreu.s.g30@gmail.com>
2025-10-18 20:21:58 +0200

    Remove production base path configuration from Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-const isProd = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   plugins: [react()],
-  base: isProd ? '/glow/' : '/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
